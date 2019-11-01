ed39ebc010d388b75b532ebbd56c3d16fcbc5db2
James <57265750+JTansey93@users.noreply.github.com>
2019-11-01 17:09:51 +0000

    Create CharacterCreation.py
    This program is designed to automate elements of the character creation process

---
+++ CharacterCreation.py
@@ -0,0 +1,16 @@
+#! python3
+import random
+
+#This function simulates rolling a six sided dice four times and adding the three highest results together
+def abilityRoll():
+    roll = []
+    for i in range(4):
+        roll.append(random.randint(1, 6))
+    roll.sort()
+    del roll[0]
+    return sum(roll)
+
+#Then we get a list of six ability scores by using the abilityRoll function and appending our results in a list
+baseAbility = []
+for i in range(6):
+    baseAbility.append(abilityRoll())
